d30368026c0bbf3b205e0b16ddad38b6ee70ebaa
mrshizzy <oziomaagugbue@gmail.com>
2021-08-10 16:57:39 -0700

    made some modifications to the config file

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 from pydantic import BaseSettings
 from typing import Optional
-import os
-from dotenv import load_dotenv
 import secrets
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
-
 class Config(BaseSettings):
-    SECRET_KEY: Optional[str] = os.getenv('SECRET_KEY', secrets.token_hex(16))
+    SECRET_KEY: Optional[str] =  secrets.token_hex(16)
     ALGORITHM: str = 'HS256'
+    
+    class Config:
+        env_file = '.env'
